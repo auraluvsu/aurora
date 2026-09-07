b697c2d85aab34dc4113cb909aff6c8a682e8611
auraluvsu <ntreasure2015@gmail.com>
2026-09-08 00:23:06 +0100

    initial commit

---
+++ files.ts
@@ -0,0 +1,9 @@
+import * as fs from "fs";
+
+fs.readFile("myfile.json", "utf8", (err, data) => {
+  if (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
+  console.log("File content:", JSON.parse(data));
+});
