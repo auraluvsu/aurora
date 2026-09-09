750f394d523c7e2fd9d052f63d2ba670994e1d32
auraluvsu <ntreasure2015@gmail.com>
2026-09-10 00:16:32 +0100

    chore: remove test file

--- src/files.ts
+++
@@ -1,9 +0,0 @@
-import * as fs from "fs";
-
-fs.readFile("myfile.json", "utf8", (err, data) => {
-    if (err) {
-        console.error("Error reading file:", err);
-        return;
-    }
-    console.log("File content:", JSON.parse(data));
-});
